bb33cce291dbb37a5ee1b76d596e17db121aacf6
Rabin Awal <robinleathal@gmail.com>
2022-01-23 13:31:45 -0600

    Added try catch blocks for handling exception on evenOrOdd method

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -6,13 +6,18 @@ public class EvenOrOdd {
 	
 	
 	public void evenOrOdd() {
-		Scanner sc = new Scanner(System.in);
-		firstNumber = sc.nextInt();
-		if (firstNumber %2 == 0) {
-			System.out.println("The number you entered " +firstNumber + " is even number");
-		} else {
-			System.out.println("The number you entered " +firstNumber + " is an odd number");
+		try (Scanner sc = new Scanner(System.in)) {
+			firstNumber = sc.nextInt();
+			if (firstNumber %2 == 0) {
+				System.out.println("The number you entered " +firstNumber + " is even number");
+			} else {
+				System.out.println("The number you entered " +firstNumber + " is an odd number");
+			}
+		} catch (Exception e){
+			e.getMessage();
+			
 		}
+		
 	}
 
 }
